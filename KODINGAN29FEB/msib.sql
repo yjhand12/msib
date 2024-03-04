94ecc53dd9c0209e0f1cc78983131cd5eb9a06fe
Firdaus Anesta Surya <131839524+yjhand12@users.noreply.github.com>
2024-03-04 14:03:12 +0700

    Add files via upload

---
+++ KODINGAN29FEB/msib.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2024 at 07:45 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `msib`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data_siswa`
+--
+
+INSERT INTO `data_siswa` (`id`, `name`, `email`, `phone`, `address`) VALUES
+(39, 'daus', 'daus@daus.com', '0812', 'SALATIGA'),
+(40, 'anesta', 'anesta@anesta.com', '2257', 'SALATIGA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
